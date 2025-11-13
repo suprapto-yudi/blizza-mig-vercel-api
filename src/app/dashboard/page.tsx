08ccd02af06bdeb0b4da8b0c9c2c4e8bcc1c4f18
suprapto-yudi <otosproperty@gmail.com>
2025-11-13 14:06:23 +0700

    feat: revisi /dashboard/page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -112,7 +112,7 @@ const DashboardContent = () => {
         if (response.ok && result.todo)
         {
             // Update UI secara optimis
-            setTodos(prevTodos => [result.data!.todo, ...prevTodos]); // Tambah di awal list
+            setTodos(prevTodos => [result.todo as Todo, ...prevTodos]); // Tambah di awal list
             setNewTodoTitle(''); 
         } else {
             alert(`Gagal membuat To-Do: ${result.message}`);
